34272fd5b7f4e850e71956e0d378ed0294c1a56e
Leigh Harrison <leigh@leightech.com>
2026-07-07 11:28:39 +1000

    Release 1.1.1

--- stack2-connector/stack2-connector.php
+++ stack2-connector/stack2-connector.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Stack2 Connector
  * Description: Sync plugin inventory to Stack2 and execute signed remote plugin commands.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Stack2
  * Requires at least: 6.0
 * Requires PHP: 7.4
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('STACK2_CONNECTOR_VERSION', '1.1.0');
+define('STACK2_CONNECTOR_VERSION', '1.1.1');
 define('STACK2_CONNECTOR_PATH', plugin_dir_path(__FILE__));
 define('STACK2_CONNECTOR_URL', plugin_dir_url(__FILE__));
 
